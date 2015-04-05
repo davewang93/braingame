f2d1ce658a4b672b48f59bf816f0bf3ac5e4aa32
dwang07 <dwang07@gmail.com>
2015-04-04 23:00:11 -0400

    image & media

--- index/views.py
+++ index/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("David says hey there world!")
+
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': "Have Fun!"}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+
+    return render(request, 'index/index.html', context_dict)
